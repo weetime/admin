dd84946bb6818c259856da299b482982d2be2c67
fangyong <fangyong@haodf.com>
2019-05-18 21:50:12 +0800

    init vue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+import '@/styles/index.scss' // global css
+
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI);
 
 new Vue({
   render: h => h(App),
